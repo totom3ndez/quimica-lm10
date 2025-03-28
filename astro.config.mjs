ad09dbcf9a5d882f20fc6154a10323a6d2bca745
totom3ndez <tomii.mendez45@gmail.com>
2025-03-28 14:17:45 +0100

    [Build] - First commit - project setted.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react()],
+  output: 'server',
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: [{
+        find: '@',
+        replacement: '/src'
+      }]
+    }
+  },
+  experimental: {
+    svg: true,
+  }
+});
