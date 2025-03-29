6e533ac6c955a61f1951d40b66eb26962cf2c1f0
totom3ndez <tomii.mendez45@gmail.com>
2025-03-29 23:49:50 +0100

    [Docs] Created content.

---
+++ src/content/config.ts
@@ -0,0 +1,23 @@
+import {z, defineCollection } from 'astro:content';
+
+const products = defineCollection({
+    loader: async () => {
+            const res = await fetch('https://script.google.com/macros/s/AKfycbwfsMysn2Y0tPu2o0IX8BkLaml2Zp1WfO253QkjpMH7vcvRhUQy9rL3WjOv2HKOroRnhQ/exec')
+            const data = await res.json()
+            return data.data.map((item: any) => ({
+                ...item,
+                id: String(item.id), // Convert to string
+                price: Number(item.price)
+            }));
+        
+    },
+    schema: z.object({
+        category: z.string().optional().default(''),
+        name: z.string(),
+        desc: z.string(),
+        price: z.number(),
+        id: z.string()
+    })
+})
+
+export const collections = {products}
