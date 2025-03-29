123843837a3bd21bce65b5283c7c5c7fda2fa6a5
totom3ndez <tomii.mendez45@gmail.com>
2025-03-30 00:13:50 +0100

    [Partial] Set fetch cache false

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,7 +2,7 @@ import {z, defineCollection } from 'astro:content';
 
 const products = defineCollection({
     loader: async () => {
-            const res = await fetch('https://script.google.com/macros/s/AKfycbwfsMysn2Y0tPu2o0IX8BkLaml2Zp1WfO253QkjpMH7vcvRhUQy9rL3WjOv2HKOroRnhQ/exec')
+            const res = await fetch('https://script.google.com/macros/s/AKfycbwfsMysn2Y0tPu2o0IX8BkLaml2Zp1WfO253QkjpMH7vcvRhUQy9rL3WjOv2HKOroRnhQ/exec', {cache: 'no-cache'})
             const data = await res.json()
             return data.data.map((item: any) => ({
                 ...item,
