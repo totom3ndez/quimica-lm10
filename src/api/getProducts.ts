55a1302ce3bc5cd47f493025efc278772c7f9db0
totom3ndez <tomii.mendez45@gmail.com>
2025-03-29 23:48:42 +0100

    [Delete] Remove unused file

--- src/api/getProducts.ts
+++
@@ -1,7 +0,0 @@
-export async function getProducts() {
-  const res = await fetch('https://script.google.com/macros/s/AKfycbzZvTF0Urf8hICBU5vZEK71zhexUbClBFGj211k_trsSivspRr2klQcGCu13islBgMPSQ/exec')
-  const data = await res.json()
-  return data
-}
-
-
